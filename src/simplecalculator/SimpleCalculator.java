6e4e3ad744d37bf11cf047afa63893c8c964ec51
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2012-11-16 18:43:46 +0100

    Verzija 1.0

---
+++ src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplecalculator;
+
+/**
+ *
+ * @author bole
+ */
+public class SimpleCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculatorForm form = new CalculatorForm();
+        form.setVisible(true);
+    }
+}
